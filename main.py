55366f86d8a950e57c6ab54e0c3acb3e894c8975
Liza V <elivolov@gmail.com>
2017-04-23 23:13:58 +0300

    Дошла до определения линий на изображении (не понятно)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+from math import pi, cos, sin
 
 # Аргументы командной строки
 
@@ -20,9 +21,12 @@ else:
     print("Введите аргументы")
     exit(0)
 
-# Загрузка изображения
-img_t = cv2.imread(template_filename)
-img_s = cv2.imread(source_filename)
+# Загрузка изображения, Изменение размера
+img = cv2.imread(template_filename)
+img_t = cv2.resize(img, (475, 310), interpolation=cv2.INTER_CUBIC)
+
+img = cv2.imread(source_filename)
+img_s = cv2.resize(img, (475, 310), interpolation=cv2.INTER_CUBIC)
 
 # Бинаризация изображения-шаблона, определение контуров изображения-шаблона
 imgray_t = cv2.cvtColor(img_t, cv2.COLOR_BGR2GRAY)
@@ -60,10 +64,54 @@ angle_mid_t = sum_angle_t/t_contours_count
 print(angle_mid_s, angle_mid_t)
 
 # 3. Тип подписи (кружевная/линейная)
-#print(cv2.HoughCircles(thresh_t, cv2.HOUGH_GRADIENT, 1, 1))
+# Поиск окружностей
+circles_t = cv2.HoughCircles(thresh_t, cv2.HOUGH_GRADIENT, 30, 10)
+for i in range(len(circles_t)):
+    for j in range(len(circles_t[i])):
+        cv2.circle(img_t, (circles_t[i, j, 0], circles_t[i, j, 1]), circles_t[i, j, 2], (0, 255, 0))
 
-cv2.drawContours(img_s, contours_s, 2, (0, 255, 0), 3)
-cv2.drawContours(img_t, contours_t, 2, (0, 255, 0), 3)
+circles_s = cv2.HoughCircles(thresh_s, cv2.HOUGH_GRADIENT, 30, 10)
+for i in range(len(circles_s)):
+    for j in range(len(circles_s[i])):
+        cv2.circle(img_s, (circles_s[i, j, 0], circles_s[i, j, 1]), circles_s[i, j, 2], (0, 255, 0))
+
+print(len(circles_t[0]), ' ', len(circles_s[0]))
+
+# Поиск линий
+edges = cv2.Canny(imgray_t,50,150,apertureSize = 3)
+
+lines_t = cv2.HoughLines(thresh_t, 20, pi/180, 100)
+
+for i in range(len(lines_t)):
+    rho = lines_t[i, 0, 0]
+    theta = lines_t[i, 0, 1]
+    a = cos(theta)
+    b = sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    cv2.line(img_t, (x1, y1), (x2, y2), (0, 0, 255), 1)
+
+minLineLength = 100
+maxLineGap = 10
+cv2.imshow("", edges)
+cv2.waitKey()
+"""lines_t = cv2.HoughLines(edges,1,pi/180,500,minLineLength,maxLineGap)
+for i in range(len(lines_t)):
+    x1 = lines_t[i, 0, 0]
+    y1 = lines_t[i, 0, 1]
+    x2 = lines_t[i, 0, 2]
+    y2 = lines_t[i, 0, 3]
+    cv2.line(img_t,(x1,y1),(x2,y2),(0,0,255),2)"""
+
+
+print(lines_t)
+
+cv2.drawContours(img_s, contours_s, -1, (0, 255, 0), 3)
+cv2.drawContours(img_t, contours_t, -1, (0, 255, 0), 3)
 
 cv2.imshow("t", img_t)
 cv2.waitKey()
